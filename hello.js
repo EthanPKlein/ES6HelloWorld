6b0ad4c591f70b59f8cc7175d2230d78c46921a1
EthanPKlein <eklein@icct.com>
2016-06-19 16:40:49 -0400

    Added goodbye!

--- hello.js
+++ hello.js
@@ -2,15 +2,14 @@ class Hello {
 	sayHello() {
 		document.querySelector("#helloDiv").innerHTML = "hello world";
 	}
-	
+
 	sayGoodbye() {
 		document.querySelector("#goodbyeDiv").innerHTML = "goodbye world";
 	}
-	
+
 }
 
 var hello = new Hello();
 hello.sayHello();
 
 hello.sayGoodbye();
-
